c5e9a1f861ec7ac52318234d0ef7ac603bed6680
Andrey Marakulin <annndruha.github@gmail.com>
2023-07-18 02:01:56 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
-from pydantic import PostgresDsn
+from pydantic import ConfigDict, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -16,8 +16,4 @@ class Settings(BaseSettings):
     PDF_PATH: str = 'userdata'
     API_VERSION: str = '5.131'
 
-    class Config:
-        """Pydantic BaseSettings config"""
-
-        case_sensitive = True
-        env_file = ".env"
+    model_config = ConfigDict(case_sensitive=True, env_file=".env", extra="allow")
